daf4e4b8b84906ade25ec411e5752c2d8bc9fedf
Braarrr <lutfiabrar02@gmail.com>
2025-08-08 14:47:29 +0700

    merubah title header

--- dashboard/index.php
+++ dashboard/index.php
@@ -23,7 +23,7 @@ $nama = $_SESSION['user']['nama'] ?? 'Pengguna';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Dashboard - Sistem Akademik SMK</title>
+  <title>Dashboard - Sistem Akademik SMK Telkom</title>
 
   <!-- Bootstrap 5 CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
